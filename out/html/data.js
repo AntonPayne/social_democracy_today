aee167f6f1f1b5c784852d49e5768d4a71ccb01b
FrancoDGamer <161732449+FrancoDGamer@users.noreply.github.com>
2026-02-11 03:53:48 +0900

    Create data.js

---
+++ out/html/data.js
@@ -0,0 +1,68 @@
+const tooltipList = [{
+    searchString: "SDP",
+    explanationText: "The Social Democratic Party of Germany. <br> (Sozialdemokratische Partei Deutschlands)"
+},
+                      {
+    searchString: "Linke",
+    explanationText: "The Left/The Left Party. <br> (Die Linke/Linkspartei)"
+},
+                     {
+    searchString: "FDP",
+    explanationText: "The Free Democratic Party. <br> (Freie Demokratische Partei)"
+},
+                     {
+    searchString: "CDU",
+    explanationText: "The Christian Democratic Union. <br> (Christlich Demokratische Union Deutschlands)"
+},
+                     {
+    searchString: "CSU",
+    explanationText: "The Christian Social Union in Bavaria. <br> (Christlich-Soziale Union in Bayern)"
+},
+                     {
+    searchString: "Greens",
+    explanationText: "Alliance 90/The Greens. <br> (Bündnis 90/Die Grünen)"
+},
+                     {
+    searchString: "Others",
+    explanationText: "A variety of other minor political parties. <br>"
+},
+                     {
+    searchString: "AfD",
+    explanationText: "Alternative for Germany. <br> (Alternative für Deutschland)"
+},
+                    
+                    ];
+
+
+const colourList = [{
+        word: "SPD",
+        style: "color: #E3000F; font-weight: bold;",
+    },
+                    {
+        word: "Linke",
+        style: "color: #8B0000; font-weight: bold;",
+    },
+                    {
+        word: "FDP",
+        style: "color: #DCCA4A; font-weight: bold;",
+    },
+                    {
+        word: "CDU",
+        style: "color: #000; font-weight: bold;",
+    },
+                    {
+        word: "CSU",
+        style: "color: #0570C9; font-weight: bold;",
+    },
+                    {
+        word: "Greens",
+        style: "color: #1AA037; font-weight: bold;",
+    },
+                    {
+        word: "Others",
+        style: "color: #a0a0a0; font-weight: bold;",
+    },
+                    {
+        word: "AfD",
+        style: "color: #0489DB; font-weight: bold;",
+    }];
